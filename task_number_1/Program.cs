e9ecd4c997cd97639f589845710f4b2726746164
user <user@WIN-0FLLSABRR5D>
2025-10-05 17:13:45 +0300

    добавила метод InputOfNumber

--- task_number_1/Program.cs
+++ task_number_1/Program.cs
@@ -2,12 +2,13 @@
 do
 {
     Console.WriteLine("Введите первое число");
-    double firstNumber = double.Parse(Console.ReadLine());
-    Console.WriteLine("Введите второе число");
-    double secondNumber = double.Parse(Console.ReadLine());
-
+    double firstNumber = InputOfNumber();
     Console.WriteLine("Введите арифметическую операцию(+, -, /, *)");
     char symbol = char.Parse(Console.ReadLine());
+    Console.WriteLine("Введите второе число");
+    double secondNumber = InputOfNumber();
+
+   
     switch (symbol)
     {
         case '+':
@@ -33,4 +34,25 @@ do
     choice = Console.ReadLine().Trim().ToLower();
 } while (choice == "Да");
 Console.WriteLine("Нажмите любую клавишу для выхода из калькулятора");
+
+static int InputOfNumber()
+{
+    string input;
+    int number=0;
+    do
+    {
+        input = Console.ReadLine();
+        if (string.IsNullOrEmpty(input))
+        {
+            Console.WriteLine("Ошибка! Вы ничего не ввели, попробуйте еще раз");
+            continue;
+        }
+        if (!int.TryParse(input, out number))
+        {
+            Console.WriteLine("Ошибка! Вы ввели не число, попробуйте еще раз");
+            continue;
+        }
+        return number;
+    }while(true);
+}
 
